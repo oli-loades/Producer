d504736396ed42d27660bf18702f5f4e42c984ee
oli <oli.loades@gmail.com>
2018-07-31 15:32:59 +0100

    corss origin

--- src/main/java/com/qa/controller/BookController.java
+++ src/main/java/com/qa/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.qa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class BookController {
 	@Autowired
 	public BookService bookService;
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/search/{name}")
 	@ResponseBody
 	public String searchBooks(@PathVariable("name") String name) {
--- src/main/java/com/qa/controller/GlossaryController.java
+++ src/main/java/com/qa/controller/GlossaryController.java
@@ -3,6 +3,7 @@ package com.qa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,13 @@ public class GlossaryController {
 	@Autowired
 	private GlossaryService glossaryService;
 	
+	  @CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/get/{keyword}")
 	public String getDefinition(@PathVariable String keyword) {
 		return glossaryService.getDefiniton(keyword);
 	}
 	
+	  @CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/getAll")
 	public List<GlossaryEntry> getAll() {
 		return glossaryService.getAll();
